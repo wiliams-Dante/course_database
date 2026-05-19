545a3c97bae79ac07952ca9eedf62e61f49f77a0
rodrigo <rsullagonzales@gmail.com>
2026-05-18 20:21:14 -0500

    estructura de la tabla empresa

---
+++ 01_ddl_estructura.sql
@@ -0,0 +1,10 @@
+/*tablitas jeje*/
+
+CREATE TABLE empresa (
+	ruc CHAR(29) primary key,
+	nombre_empresa VARCHAR(30) not null,
+	direccion_empresa VARCHAR(50) null,
+	telefono_empresa BIGINT not null,
+	placa_unidad VARCHAR(6) not null,
+	chofer_id CHAR(14) not null
+);
