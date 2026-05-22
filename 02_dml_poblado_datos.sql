97861a8a7bc5f011694c0d0e933e1a0ad512ff85
ALUMNO D102 <ALUMNO-D102@UCSP.AULAB>
2026-05-22 09:58:43 -0500

    feat: dato 02: tabla pasajeros subida

--- 02_dml_poblado_datos.sql
+++ 02_dml_poblado_datos.sql
@@ -1,4 +1,5 @@
 /*insertando tablas independientes*/
 INSERT INTO pasajero (pasajero_id, dni_pasajero, telefono_pasajero)
-VALUES ('pasaj0000000001', '60062056', '983104472');
-
+VALUES 
+	('pasa0000000001', '60062056', '983104472'),
+	('pasa0000000002', "72385379', '982292240');
