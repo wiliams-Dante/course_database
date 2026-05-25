2f386d56941bfe79385965d8629037ef047c3b04
rodrigo <rsullagonzales@gmail.com>
2026-05-24 21:51:25 -0500

    feat: datos tabla pasajero

--- 02_dml_poblado_datos.sql
+++ 02_dml_poblado_datos.sql
@@ -1,4 +1,8 @@
 /*insertando tablas independientes*/
-INSERT INTO pasajero (pasajero_id, dni_pasajero, telefono_pasajero)
-VALUES ('pasaj0000000001', '60062056', '983104472');
-
+INSERT INTO pasajero (pasajero_id, dni_pasajero, telefono_pasajero) 
+VALUES 
+	('PSJ00000000001', '60062056', '983104472'),
+	('PSJ00000000002', '72345678', '912345678'),
+	('PSJ00000000003', '73456789', '923456789'),
+	('PSJ00000000004', '74567890', '934567890'),
+	('PSJ00000000005', '75678901', '945678901');
