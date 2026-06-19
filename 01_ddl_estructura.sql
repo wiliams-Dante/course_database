b3e9bd23528c917825a8b8623f2983f01c22ad2c
Tavogus0117 <gustavo.moreno@ucsp.edu.pe>
2026-06-18 20:37:14 -0500

    refactor: agregar validacion check de montos y estados en la tabla de transaccion

--- 01_ddl_estructura.sql
+++ 01_ddl_estructura.sql
@@ -140,3 +140,7 @@ ALTER TABLE ruta
 ALTER TABLE unidad 
     ADD CONSTRAINT chk_capacidad CHECK (capacidad_psjros > 0 AND capacidad_psjros <= 100),
     ADD CONSTRAINT chk_estado_unidad CHECK (estado_operativo IN ('Operativo', 'Mantenimiento', 'Inactivo'));
+
+ALTER TABLE transaccion 
+    ADD CONSTRAINT chk_monto_tr CHECK (monto_tr > 0),
+    ADD CONSTRAINT chk_estado_tr CHECK (estado_tr IN ('Completada', 'Rechazada', 'Anulada'));
