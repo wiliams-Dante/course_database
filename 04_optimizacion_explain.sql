be02916a18a03e5b527fe44471c0b42ea058cdf0
rodrigo <rsullagonzales@gmail.com>
2026-06-19 22:43:22 -0500

    feat: deje la base ed datos limpia e inserte 20 transacciones aleatorias para comenzar con la prueba del explain analyze

--- 04_optimizacion_explain.sql
+++ 04_optimizacion_explain.sql
@@ -26,3 +26,18 @@ SELECT
     'PSJ' || LPAD(i::text, 11, '0'),
     'TRJ' || LPAD(i::text, 11, '0')
 FROM generate_series(100, 15000) AS i;
+
+DELETE FROM transaccion WHERE transaccion_id > 'TRN00000000005';
+
+--transacciones
+INSERT INTO transaccion (transaccion_id, monto_tr, fecha_hora_tr, estado_tr, pasajero_id, tarjeta_id, tarifa_id)
+SELECT 
+    'TRN' || LPAD(i::text, 11, '0'),
+    floor(random() * 5 + 1)::INT,
+    NOW() - (random() * 365 || ' days')::interval,
+    CASE WHEN random() > 0.1 THEN 'Completada' ELSE 'Rechazada' END,
+    --aqui usamos modulo para asegurar que el id exista en lastaablas anteriores
+    'PSJ' || LPAD((100 + (i % 14900))::text, 11, '0'),
+    'TRJ' || LPAD((100 + (i % 14900))::text, 11, '0'),
+    'TRF' || LPAD((100 + (i % 14900))::text, 11, '0')
+FROM generate_series(100, 20000) AS i;
